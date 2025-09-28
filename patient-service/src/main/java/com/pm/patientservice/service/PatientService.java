988e4c8c6610413e0c1c44339af200aeffb0b07c
Rudra1723 <patel.rudraa@northeastern.edu>
2025-09-28 14:19:20 -0400

    Added Service Package

---
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -0,0 +1,21 @@
+package com.pm.patientservice.service;
+
+import java.util.List;
+
+import com.pm.patientservice.model.Patient;
+import com.pm.patientservice.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PatientService {
+    private PatientRepository patientRepository;
+
+
+    public void PatientRepository(PatientRepository patientRepository){
+        this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatient(){
+        List<Patient> patients = patientRepository.findAll();
+    }
+}
