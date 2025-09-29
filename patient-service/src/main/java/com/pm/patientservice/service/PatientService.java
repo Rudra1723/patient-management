d9af009e2c9b07c97371a21eb82173239b824c76
Rudra1723 <patel.rudraa@northeastern.edu>
2025-09-29 16:58:30 -0400

    Implemented delete-patient End-Points

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -57,4 +57,10 @@ public class PatientService {
         return PatientMapper.toDTO(updatedPatient);
 
     }
+
+    public void deletePatient(UUID id) {
+        patientRepository.deleteById(id);
+    }
+
+
 }
