e1aadd66e38935b8fbc47ce6e399745d360f9d41
Rudra1723 <patel.rudraa@northeastern.edu>
2025-10-06 13:12:34 -0400

    Implemeneted SecurityConfig and passwordEncoder

--- auth-service/src/main/java/com/pm/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/pm/authservice/service/AuthService.java
@@ -3,6 +3,7 @@ package com.pm.authservice.service;
 import com.pm.authservice.model.User;
 import com.pm.authservice.dto.LoginRequestDTO;
 import com.pm.authservice.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -10,10 +11,13 @@ import java.util.Optional;
 @Service
 public class AuthService {
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthService(UserService userService) {
+    public AuthService(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
+
 
     public Optional<String> authenticate(LoginRequestDTO loginRequestDTO) {
         Optional<String> token = userService.findByEmail(loginRequestDTO.getEmail())
