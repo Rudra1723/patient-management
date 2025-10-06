8e7d0e6a0b110e0d30f60fbe70f21b9aba0e72e8
Rudra1723 <patel.rudraa@northeastern.edu>
2025-10-06 14:24:34 -0400

    Validate token-endpoint and token-logic

--- auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
+++ auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
@@ -1,9 +1,12 @@
 package com.pm.authservice.util;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
@@ -29,5 +32,19 @@ public class JwtUtil {
                 .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
                 .signWith(secretKey)
                 .compact();
+    }
+
+    public void validateToken(String token) {
+        try{
+            Jwts.parser().verifyWith((SecretKey) secretKey)
+                    .build()
+                    .parseSignedClaims(token);
+
+        } catch (SignatureException e) {
+            throw new JwtException("Invalidate Jwt Token");
+        } catch (JwtException e){
+            throw new JwtException("Invalid Jwt ");
+
+        }
     }
 }
