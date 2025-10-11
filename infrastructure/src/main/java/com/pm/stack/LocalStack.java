fc7b0ed6d3b0f63ea6e520e11daf4b1d78ac550c
Rudra1723 <patel.rudraa@northeastern.edu>
2025-10-11 11:04:07 -0400

    Infrastructure-project-setup and created first stack

---
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -0,0 +1,19 @@
+package com.pm.stack;
+
+import software.amazon.awscdk.*;
+
+public class LocalStack extends Stack {
+    public LocalStack(final App scope, final String ID, final StackProps props) {
+     super(scope, ID, props);
+    }
+    public static void main(final String[] args) {
+        App app = new App(AppProps.builder().outdir("./cdk.out").build());
+        StackProps props = StackProps.builder()
+                .synthesizer(new BootstraplessSynthesizer())
+                .build();
+
+        new LocalStack(app,  "localstack", props );
+        app.synth();
+        System.out.println("App synth is in Progess");
+    }
+}
