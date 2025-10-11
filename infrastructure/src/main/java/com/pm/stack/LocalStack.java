b0cac77b09ccddf2220448a1afbf5c68accbfd04
Rudra1723 <patel.rudraa@northeastern.edu>
2025-10-11 11:21:49 -0400

    Created dbs

--- infrastructure/src/main/java/com/pm/stack/LocalStack.java
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -1,7 +1,11 @@
 package com.pm.stack;
 
 import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.ec2.InstanceClass;
+import software.amazon.awscdk.services.ec2.InstanceSize;
+import software.amazon.awscdk.services.ec2.InstanceType;
 import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.rds.*;
 
 public class LocalStack extends Stack {
     private final Vpc vpc;
@@ -9,6 +13,12 @@ public class LocalStack extends Stack {
      super(scope, ID, props);
 
      this.vpc = createVpc();
+
+     DatabaseInstance authServiceDb =
+                createDatabase("AuthServiceDB", "auth-service-db");
+
+     DatabaseInstance patientServiceDb =
+                createDatabase("PatientServiceDB", "patient-service-db");
     }
 
     private Vpc createVpc() {
@@ -18,6 +28,23 @@ public class LocalStack extends Stack {
                 .maxAzs(2)
                 .build();
     }
+
+    private DatabaseInstance createDatabase(String id, String dbName){
+        return DatabaseInstance.Builder
+                .create(this, id)
+                .engine(DatabaseInstanceEngine.postgres(
+                        PostgresInstanceEngineProps.builder()
+                                .version(PostgresEngineVersion.VER_17_2)
+                                .build()))
+                .vpc(vpc)
+                .instanceType(InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO))
+                .allocatedStorage(20)
+                .credentials(Credentials.fromGeneratedSecret("admin_user"))
+                .databaseName(dbName)
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .build();
+    }
+
     public static void main(final String[] args) {
         App app = new App(AppProps.builder().outdir("./cdk.out").build());
         StackProps props = StackProps.builder()
